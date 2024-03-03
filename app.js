7115967514de4d4c8c423035ee3f0bf0781bc405
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-03-04 01:59:57 +0600

    trying lets discuss section

---
+++ app.js
@@ -0,0 +1,9 @@
+
+
+const allPosts = async() =>{
+    const res = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts");
+    const data = await res.json();
+    console.log(data);
+}
+
+allPosts();
