5ecc847a4970ba75fcf4a8111004998ed429a335
Najmulsp <mdnajmulsarker1595@gmail.com>
2024-03-04 23:51:06 +0600

    finished projects

--- app.js
+++ app.js
@@ -1,144 +1,202 @@
-const allPosts = async() =>{
-    const res = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts");
-    const data = await res.json();
-    
-    const info =data.posts;
-    console.log(info);
-    displayInfo(info);
+const discussContainer = document.getElementById('discuss-container');
+const markContainer = document.getElementById('mark-post')
+const latestPostContainer = document.getElementById('latest-post-container');
+
+
+const allPosts = async () => {
+  const res = await fetch("https://openapi.programming-hero.com/api/retro-forum/posts");
+  const data = await res.json();
+  const info = data.posts;
+  displayInfo(info);
 }
 
-    const displayInfo = (info) =>{
-    // console.log(info)
-    // step-1: get the container
-    const discussContainer = document.getElementById('discuss-container');
-    discussContainer.classList =`bg-red-100 flex flex-col justify-between gap-6`;
-    info.forEach(card =>{
-    // console.log(card)
-    // step-2: create a div as card container
-    const infoCard =document.createElement('div'); 
-    infoCard.classList = ` bg-red-100 flex flex-col lg:flex-row justify-between gap-6`;
-    // step-3: set the inner html
-    infoCard.innerHTML = `
-    <div class="bg-orange-200  flex flex-col lg:flex-row justify-between  rounded-2xl ">
-            <div class="w-16 mx-auto m-6 lg:ml-6"> <img class="mx-auto" src="${card.image}" alt=""></div>
-            <div class="flex-1 p-10 space-y-4">
-                <h2 class="font-semibold"># ${card.category} &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp &nbsp<span>Authore: &nbsp ${card?.author?.name}</span></h2> 
-                <h1 class="text-xl font-bold">${card.title}</h1>
-                <p>${card.description}
-                </p>
-                <div class="divider"></div>
+const displayInfo = (info) => {
+  console.log(info);
 
 
-            <div class="flex items-center">
-                <div class="flex-1 flex gap-12 lg:gap-6 border  ">
-                    <div class="flex flex-col lg:flex-row items-center lg:gap-4">
-                        <img class="w-10" src="images/message1.svg" alt="">
-                        <span class="lg:mr-8">${card.comment_count}</span>
-                    </div>
-                    <div class="flex flex-col lg:flex-row lg:gap-4 items-center">
-                        <img class="w-10" src="images/eye.svg" alt="">
-                        <span class="lg:mr-8">${card.view_count}</span>
-                     </div>
-                     <div class="flex flex-col lg:flex-row lg:gap-4 items-center">
-                        <img class="w-10" src="images/clock.png" alt="">
-                        <span class="lg:mr-8"> ${card.posted_time} min</span>
 
-                     </div>
+  info.forEach(post => {
+
+    const infoCard = document.createElement('div');
+    infoCard.classList = 'bg-[#797DFC1A] rounded-3xl mb-10';
+
+    console.log(post);
+
+    if (post.isActive) {
+      infoCard.innerHTML = `
+ <div class="flex flex-col lg:flex-row p-10 gap-6">
+
+     <div class="avatar relative">
+                  <div id="status" class="w-4 h-4 bg-green-600 absolute top-[-4px] left-[70px] lg:top-[-4px] lg:right-[-4px]   rounded-full"></div>
+                  <div class="w-20 h-20 rounded-xl">
+                    <img src="${post.image}" />
+                  </div>
                 </div>
-                <div>
-                    <img id="message" onclick="showName(a)" class="w-10" src="images/message.svg" alt="">
-                </div>
-            </div>
-            
+             
                 
-            </div>
-        </div>
-
-        
-        
-    `;
-
-// mark showing container
-const Nam =  document.getElementById('message');
-const Nam = (title, view) => {
-    const mark = document.createElement('div');
-    mark.classList = 'px-5'
-    mark.innerHTML = `
-    <div class="flex my-4  mx-auto rounded-xl pl-2 py-2 justify-between bg-white">
-                      <div class="font-mulish my-3 text-left font-bold text-xl">${title}</div>
-                      <div class="flex items-center gap-2 pr-2">
-                      <img src="images/eye.svg" alt=""><span>${view}</span>
+               <div class="w-full">
+                  <div class="flex justify-start  gap-5 font-medium font-inter">
+                    <div class="flex flex-col gap-2 lg:gap-5 lg:flex-row">
+                    <div>
+                      #${post.category}
+                    </div>
+                    <div><span>Author :</span>
+                      ${post?.author?.name || 'Unknown'}
+                    </div>
+                    </div>
+                  </div>
+                  <div class="font-mulish my-3 text-left font-bold text-xl">${post.title}
+                  </div>
+                  <p class="font-inter">${post.description}</p>
+                  <hr class="border-dashed border-[1px] border-gray-400 my-3">
+                  <div class="flex mt-3 justify-between items-center">
+                    <div class="flex gap-3 lg:gap-8 justify-around">
+                      <div>
+                        <i class="fa-regular fa-message lg:mr-2"></i> <span>${post.comment_count}</span>
+                      </div>
+                      <div>
+                        <i class="fa-solid fa-eye lg:mr-2"></i><span>${post.view_count}</span>
+                      </div>
+                      <div>
+                        <i class="fa-regular fa-clock lg:mr-2"></i> <span>${post.posted_time} min</span>
                       </div>
                     </div>
-    `
-    Nam.appendChild(mark);
-    readCount();
-  }
-  let count = 0;
-const readCount = () => {
+                    <div>
+                      <button onclick="markAsRead(&#34 ${post.title} &#34, '${post.view_count}')" id="mark-as-read" class="mark-read text-white py-1 px-2 rounded-full bg-green-400"><i
+                          class="fa-solid fa-envelope"></i></button>
+                    </div>
+
+                  </div>
+
+                </div>
+              </div>
+      `;
+    }
+    else {
+
+      infoCard.innerHTML = `
+ <div class="flex flex-col lg:flex-row p-10 gap-6">
+
+     <div class="avatar relative">
+                  <div id="status" class="w-4 h-4 bg-red-600 absolute top-[-4px] left-[70px] lg:top-[-4px] lg:right-[-4px]   rounded-full"></div>
+                  <div class="w-20 h-20 rounded-xl">
+                    <img src="${post.image}" />
+                  </div>
+                </div>
+             
+                
+               <div class="w-full">
+                  <div class="flex justify-start  gap-5 font-medium font-inter">
+                    <div class="flex flex-col gap-2 lg:gap-5 lg:flex-row">
+                    <div>
+                      #${post.category}
+                    </div>
+                    <div><span>Author :</span>
+                      ${post?.author?.name || 'Unknown'}
+                    </div>
+                    </div>
+                  </div>
+                  <div class="font-mulish my-3 text-left font-bold text-xl">${post.title}
+                  </div>
+                  <p class="font-inter">${post.description}</p>
+                  <hr class="border-dashed border-[1px] border-gray-400 my-3">
+                  <div class="flex mt-3 justify-between items-center">
+                    <div class="flex gap-3 lg:gap-8 justify-around">
+                      <div>
+                        <i class="fa-regular fa-message lg:mr-2"></i> <span>${post.comment_count}</span>
+                      </div>
+                      <div>
+                        <i class="fa-solid fa-eye lg:mr-2"></i><span>${post.view_count}</span>
+                      </div>
+                      <div>
+                        <i class="fa-regular fa-clock lg:mr-2"></i> <span>${post.posted_time} min</span>
+                      </div>
+                    </div>
+                    <div>
+                      <button onclick="markAsRead(&#34 ${post.title} &#34, '${post.view_count}')" id="mark-as-read" class="mark-read text-white py-1 px-2 rounded-full bg-green-400"><i
+                          class="fa-solid fa-envelope"></i></button>
+                    </div>
+
+                  </div>
+
+                </div>
+              </div>
+      `;
+    }
+
+
+
+
+
+    discussContainer.appendChild(infoCard);
+
+  })
+}
+
+
+const markAsRead = (title, view) => {
+
+  const mark = document.createElement('div');
+  mark.classList = 'px-5'
+  mark.innerHTML = `
+  <div class="flex my-4  mx-auto rounded-xl pl-2 py-2 justify-between bg-white">
+                    <div class="font-mulish my-3 text-left font-bold text-xl">${title}</div>
+                    <div class="flex items-center gap-2 pr-2">
+                      <i class="fa-solid fa-eye"></i><span>${view}</span>
+                    </div>
+                  </div>
+  `
+  markContainer.appendChild(mark);
+  getCount();
+}
+
+
+
+let count = 0;
+const getCount = () => {
   count++;
   const readCount = document.getElementById('read-count');
-  readCount.innerText = `(${count})`;
-}
-        
-         //  step-4: append child
-         discussContainer.appendChild(infoCard);
-// show name onclick
-    //    const asider =  document.getElementById('message');
-    //    asider.addEventListener('click',function(){
-    //     const div =document.createElement('div')
-    //         div.classList = `flex justify-between gap-10 bg-white rounded-2xl p-4 w-96`;
-    //         div.innerHTML = `
-
-    //          `
-    //    })
-
-                })
-                }
-
-       
-
-
-        //  function showName(){
-        //     const div =document.createElement('div')
-        //     div.classList = `flex justify-between gap-10 bg-white rounded-2xl p-4 w-96`;
-        //     div.innerHTML = `
-
-        //     `
-            
-        // }
-
-
-
-
-        // letest post section
-
-const letestPosts = async() =>{
-    const res = await fetch(" https://openapi.programming-hero.com/api/retro-forum/latest-posts");
-    const data = await res.json();
-    console.log(data)
-    displaydata(data);
+  readCount.innerText = ('${count}');
 }
 
-    const displaydata = (data) =>{
-     // console.log(data)
-     // step-1: get the container
-     const postContainer = document.getElementById('post-container');
-     postContainer.classList=`grid grid-cols-1 lg:grid-cols-3 gap-6`
 
-     data.forEach(post =>{
-    // console.log(post)
-     // step-2: create a div as post container
-     const postCard =document.createElement('div'); 
-    //  postCard.classList = `grid grid-cols-1 lg:grid-cols-3 gap-6`;
-     // step-3: set the inner html
+
+
+
+
+
+
+
+
+
+// // letest post section
+
+const letestPosts = async () => {
+  const res = await fetch(" https://openapi.programming-hero.com/api/retro-forum/latest-posts");
+  const data = await res.json();
+  console.log(data)
+  displaydata(data);
+}
+
+const displaydata = (data) => {
+  // console.log(data)
+  // step-1: get the container
+
+
+  data.forEach(post => {
+    // // console.log(post)
+    // step-2: create a div as post container
+    const postCard = document.createElement('div');
+    postCard.classList = 'card w-full bg-base-100 shadow-xl';
+
+    // step-3: set the inner html
     postCard.innerHTML = `
-     <div class="card w-96 bg-base-100 shadow-xl">
+     
                 <figure><img src="${post.cover_image}" alt="Shoes" /></figure>
                 <div class="card-body">
                   <div class="flex gap-6">
                     <img src="images/Frame.svg" alt="">
-                    <p>${post.author.posted_date || 'No Publish Date'}</p>
+                    <p>${post.author?.posted_date || 'No Publish Date'}</p>
                   </div>
                   <h1 class="font-semibold">${post.title}</h1>
                   <p>${post.description}</p>
@@ -147,37 +205,18 @@ const letestPosts = async() =>{
                     <img class="w-12 rounded-full" src="${post.profile_image}" alt="">
                     <div>
                         <h3>${post.author.name}</h3>
-                        
-                        <p>${post.author.designation || 'Unknown'}</p>
+
+                        <p>${post.author?.designation || 'Unknown'}</p>
                     </div>
                   </div>
                 </div>
-     </div>`;
-          //  step-4: append child
-          postContainer.appendChild(postCard);
+     `;
+    //  step-4: append child
+    latestPostContainer.appendChild(postCard);
 
-                 })
-                }
+  })
+}
 
-                
-         allPosts();
-        letestPosts();
+letestPosts();
 
-
-
-
-
-        const searchHandle = () => {
-            const inputField = document.getElementById('search-field');
-            const inputText = inputField.value.toLowerCase();
-          
-            if (inputText.length === 0) {
-              alert('Please type something for search');
-            }
-          
-            spinner.classList.remove('hidden');
-            allPostContainer.classList.add('hidden');
-            categoryPostContainer.classList.remove('hidden');
-            noResult.classList.add('hidden');
-            setTimeout(DisplayBySearch, 2000, inputText);
-          }
+allPosts();
